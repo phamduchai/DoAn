99612c5e614f65ea7f6afc65d9a75d319c1c185b
MINHPHUONG1409 <nguyenminhphuong1409@gmail.com>
2023-10-30 06:24:50 +0700

    DoAn

---
+++ WebApplication1/SliderJVS.js
@@ -0,0 +1,48 @@
+﻿const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let isMouseOver = false;
+const sliderContainer = document.querySelector('.slider-container');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    }
+    const translateX = -currentIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+sliderContainer.addEventListener('mouseenter', () => {
+    isMouseOver = true;
+    prevButton.style.display = 'block';
+    nextButton.style.display = 'block';
+});
+
+
+sliderContainer.addEventListener('mouseleave', () => {
+    isMouseOver = false;
+    setTimeout(() => {
+        if (!isMouseOver) {
+            prevButton.style.display = 'none';
+            nextButton.style.display = 'none';
+        }
+    }, 0);
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex--;
+    showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+    showSlide(currentIndex);
+});
+
+
+showSlide(currentIndex);
